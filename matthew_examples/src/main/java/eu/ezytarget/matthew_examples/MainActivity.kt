9f59c8bcf2ba1da8eaf3d9be3164a5bdad4a5270
Michel Sievers <michel@easy-target.eu>
2019-08-15 00:38:33 +0200

    Configured kotlin in lib module. First impl of lib in example.

--- matthew_examples/src/main/java/eu/ezytarget/matthew_examples/MainActivity.kt
+++ matthew_examples/src/main/java/eu/ezytarget/matthew_examples/MainActivity.kt
@@ -1,39 +1,26 @@
 package eu.ezytarget.matthew_examples
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity;
-import android.view.Menu
-import android.view.MenuItem
-
+import androidx.appcompat.app.AppCompatActivity
+import eu.ezytarget.matthew.Matthew
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var matthew: Matthew = Matthew()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        fab.setOnClickListener {
+            generateExample()
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
+    private fun generateExample() {
+        val color = 0xFFFF00FF.toInt()
+        matthew.fillCanvas(color)
     }
 }
