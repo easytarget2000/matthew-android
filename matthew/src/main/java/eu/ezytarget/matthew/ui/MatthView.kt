8fdca404063a7216abd3bb9bcc51524d8c4e8882
Michel Sievers <michel@easy-target.eu>
2019-08-16 15:28:11 +0200

    Added onDraw() logging.

--- matthew/src/main/java/eu/ezytarget/matthew/ui/MatthView.kt
+++ matthew/src/main/java/eu/ezytarget/matthew/ui/MatthView.kt
@@ -3,12 +3,15 @@ package eu.ezytarget.matthew.ui
 import android.content.Context
 import android.graphics.Canvas
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
+import eu.ezytarget.matthew.BuildConfig
 import eu.ezytarget.matthew.Matthew
 
 class MatthView: View {
 
     var matthew: Matthew
+    var verbose = BuildConfig.DEBUG
 
     @JvmOverloads constructor(
         context: Context,
@@ -20,10 +23,18 @@ class MatthView: View {
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+        if (verbose) {
+            Log.d(MatthView.tag, "onDraw(): canvas: $canvas")
+        }
+
         if (canvas == null) {
             return
         }
 
         matthew.drawSeeded(canvas)
+    }
+
+    companion object {
+        val tag: String = Matthew::class.java.simpleName
     }
 }
