e75dbc99ee858af09a6dad5fd733e2d58f0e9b6d
Michel Sievers <michel@easy-target.eu>
2019-09-28 23:57:13 +0200

    Tweak converging cards variables into first reasonable implementation

--- matthew/src/main/java/eu/ezytarget/matthew/pattern/ConvergingCardsMatthewPattern.kt
+++ matthew/src/main/java/eu/ezytarget/matthew/pattern/ConvergingCardsMatthewPattern.kt
@@ -3,37 +3,37 @@ package eu.ezytarget.matthew.pattern
 import android.graphics.Canvas
 import eu.ezytarget.matthew.Matthew
 import eu.ezytarget.matthew.util.RandomNumberGenerator
+import kotlin.math.max
 
 class ConvergingCardsMatthewPattern(
     private val randomNumberGenerator: RandomNumberGenerator = RandomNumberGenerator()
 ) {
     var numberOfCards = 10
-    var initialDegrees = -40f
-    var deltaDegrees = -1f
+    var initialDegrees = -45f
+    var deltaDegrees = -2f
+    var changeColorProbability = 0.4f
 
     fun configureRandomly() {
-        numberOfCards = randomNumberGenerator.int(8, 12)
-//        initialDegrees = randomNumberGenerator.float(-30f, -60f)
-        deltaDegrees = 5f//randomNumberGenerator.float(-1f, 1f)
+        numberOfCards = randomNumberGenerator.int(4, 10)
+        initialDegrees = -randomNumberGenerator.float(30f, 50f)
+        deltaDegrees = randomNumberGenerator.float(-3f, 3f)
+        changeColorProbability = randomNumberGenerator.float(0.1f, 0.5f)
     }
-
+ø
     fun paintRandomly(matthew: Matthew, canvas: Canvas) {
-        val minCardWidth = canvas.width / numberOfCards.toFloat()
-        val maxCardWidth = canvas.width / 4f
+        val longestCanvasSideLength = max(canvas.width, canvas.height)
+        val minCardWidth = longestCanvasSideLength / numberOfCards.toFloat()
+        val maxCardWidth = minCardWidth * 3f
 
-        val cardHeight = canvas.height * 2f
-        val rectangleTop = 0f - (cardHeight / 4f)
-        var rectangleLeft = canvas.width.toFloat()
+        val cardHeight = longestCanvasSideLength * 3f
+        val rectangleTop = 0f - (cardHeight / 2f)
+        var rectangleLeft = canvas.width.toFloat() * 2f
         var color = matthew.colorAtModuloIndex(0)
 
         for(cardCounter in numberOfCards downTo 0) {
             val cardWidth = randomNumberGenerator.float(minCardWidth, maxCardWidth)
             rectangleLeft -= cardWidth
             val rectangleDegrees = initialDegrees + (cardCounter * deltaDegrees)
-            val changeColor = randomNumberGenerator.boolean(trueProbability = 0.7f)
-            if (changeColor) {
-                color = matthew.colorAtModuloIndex(cardCounter)
-            }
 
             matthew.paintRectangle(
                 rectangleLeft,
@@ -44,6 +44,11 @@ class ConvergingCardsMatthewPattern(
                 color,
                 canvas
             )
+
+            val changeColor = randomNumberGenerator.boolean(changeColorProbability)
+            if (changeColor) {
+                color = matthew.colorAtModuloIndex(cardCounter)
+            }
         }
 
     }
